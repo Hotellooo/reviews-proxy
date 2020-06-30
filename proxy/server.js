7ee5ea079f5df9a0abdc7c6d9bc7c92bcae953ab
gcking <george.king823@gmail.com>
2020-06-29 20:55:06 -0700

    Set up reverse proxy for Reviews component

--- proxy/server.js
+++ proxy/server.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const httpProxy = require('http-proxy');
 const port = process.env.PORT || 2999;
+const apiProxy = httpProxy.createProxyServer();
 
+// const calendarServer = 'http://localhost:3001';
+// const photosServer = 'http://localhost:3002';
+// const aboutServer = 'http://localhost:3003';
+const reviewsServer = 'http://localhost:3004';
+
+// CALENDAR
+
+
+// PHOTOS
+
+
+// ABOUT
+
+
+// REVIEWS
+app.all('/reviews', (req, res) => {
+  console.log('redirecting to reviews server');
+  apiProxy.web(req, res, {target: reviewsServer, changeOrigin: true});
+});
+
+app.all('/reviews/*', (req, res) => {
+  console.log('redirecting to reviews server');
+  apiProxy.web(req, res, {target: reviewsServer, changeOrigin: true});
+});
+
+
+// ALL
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => console.log(`Server is running at port ${port}`));
