1f47641fc915b9599b0c27ee90cfd0543849bb87
gcking <george.king823@gmail.com>
2020-06-29 21:58:16 -0700

    Running Reviews and Calendar on proxy. Had to npm install cors in Calendar's server file in order to avoid CORS errors.

--- proxy/server.js
+++ proxy/server.js
@@ -4,14 +4,31 @@ const app = express();
 const httpProxy = require('http-proxy');
 const port = process.env.PORT || 2999;
 const apiProxy = httpProxy.createProxyServer();
+const cors = require('cors');
+app.use(cors());
 
-// const calendarServer = 'http://localhost:3001';
+const calendarServer = 'http://localhost:3001';
 // const photosServer = 'http://localhost:3002';
 // const aboutServer = 'http://localhost:3003';
 const reviewsServer = 'http://localhost:3004';
 
+
+
 // CALENDAR
 
+// / <-- static
+
+app.all('/api/calendar/db/*', (req, res) => {
+  console.log('redirecting to calendar server');
+  apiProxy.web(req, res, {target: calendarServer, changeOrigin: true});
+});
+
+app.all('/api/calendar/update/', (req, res) => {
+  console.log('redirecting to calendar server');
+  apiProxy.web(req, res, {target: calendarServer, changeOrigin: true});
+});
+
+// /api/calendar/update/
 
 // PHOTOS
 
@@ -20,11 +37,6 @@ const reviewsServer = 'http://localhost:3004';
 
 
 // REVIEWS
-app.all('/reviews', (req, res) => {
-  console.log('redirecting to reviews server');
-  apiProxy.web(req, res, {target: reviewsServer, changeOrigin: true});
-});
-
 app.all('/reviews/*', (req, res) => {
   console.log('redirecting to reviews server');
   apiProxy.web(req, res, {target: reviewsServer, changeOrigin: true});
